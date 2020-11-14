61f34f023d8ef55deb54953390a1bc80c615fdc0
Jonathan Crecencio <jonathancrecencio@outlook.com>
2020-11-14 20:28:30 -0300

    Recriando a interface

---
+++ js/pages-stores.js
@@ -0,0 +1,69 @@
+//Create Map
+function geoLocation(){
+    if('geolocation' in navigator){
+        navigator.geolocation.getCurrentPosition(function(position){
+            const latitude = position.coords.latitude
+            const longitude = position.coords.longitude
+            const map = L.map('mapid').setView([latitude, longitude], 12);
+
+            //Create and add tileLayer
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+            console.log(`a latitude ${latitude} e a longitude ${longitude}`)
+
+
+            const storeSpan = document.querySelectorAll('#store')
+            const dataSpan = [
+                {
+                    id:"1",
+                    name: "Loja1 - Santos Praia Mar",
+                    lat: "-23.976896",
+                    lon: "-46.311185"
+                },
+
+                {
+                    id:"2",
+                    name: "Loja2 - Santos Gonzaga",
+                    lat: "-23.967227",
+                    lon: "-46.334464",
+                },
+
+                {
+                    id:"3",
+                    name: "Loja3 - Praia Grande",
+                    lat: "-23.997473",
+                    lon: "-46.407459",
+                }
+        ]
+            dataSpan.forEach(span => {
+                storeSpan.innerHTML += `<span hidden
+                data-id="${span.id}"
+                data-name="${span.name}"
+                data-lat="${span.lat}"
+                data-lon="${span.lon}"
+                ></span>`
+
+                //create icon
+                const icon = L.icon({
+                    iconUrl: "image/logo.svg",
+                    iconSize: [58, 68],
+                    iconAnchor: [29, 68],
+                    popupAnchor:[170, 2]
+                })
+                const popup = L.popup({
+                    closeButton: false,
+                    className: 'map-popup',
+                    minWidth: 240,
+                    minHeight: 240
+                }).setContent(`${span.name}<a href="/showcase${span.id}.html"><img src="image/arrow.svg"></a>`)
+
+                L.marker([span.lat, span.lon], {icon}).addTo(map).bindPopup(popup)
+            })
+
+        }, function(error){
+            console.log(error)
+        })
+    }else{
+        alert('ops não foi possivel pegar a localização')
+    }   
+}
+geoLocation()
---
+++ readme.md
@@ -0,0 +1,33 @@
+    # Este software é somente para mostrar uma solução.
+    ## Template baseado no Happy da NLW3
+    Como proposto, necessitavamos desenvolver uma solução sobre algum problema específico de uma grande loja, o problema seria, melhorar o atendimento ao cliente e a mobilidade. Com base em pesquisas via reclame aqui, verificamos que existem muitas reclamações quanto a entrega expressa que possui seu prazo de 2 dias úteis.
+
+    Pensando nessa reclamação surgiu.. O Flash.
+
+    
+    ### O que é o Flash?
+
+    Flash é uma plataforma de geolocalização, onde o cliente consegue visualizar a loja próxima a ele e efetuar a sua compra diretamente daquela loja, e com isso, é possível uma entrega de 3 horas úteis, para este método que existe a geolocalização do navegador, para mapear apenas as lojas dentro do raio de quilometragem.
+
+
+    ### Como visualizar o flash em meu computador neste momento ?
+
+    Para este caso, é necessário abrir dentro de um editor de texto (VSCode de preferência), utilizar a extensão live server,neste processo, ele pegará a sua geolocalização (pedirá uma autorização para utilizar), como não temos acesso ao banco de dados de todas as lojas, a localização de lojas próximas estão dentro do raio da minha residência.
+
+    Abaixo algumas imagens da aplicação.
+
+    
+    ![Área inicial](/image/Slide1.JPG)
+    ![Localização das lojas](/image/Slide2.JPG)
+    ![Marcação das lojas](/image/Slide3.JPG)
+    ![Loja virtual (demonstração)](/image/Slide4.JPG)
+    ![Loja virtual (demonstração)](/image/Slide5.JPG)
+    ![Loja virtual (demonstração)](/image/Slide6.JPG)
+    ![Loja virtual (demonstração)](/image/Slide7.JPG)
+
+
+
+
+
+
+
